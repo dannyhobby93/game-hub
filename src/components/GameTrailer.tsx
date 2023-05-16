b7f8bbb7c5b37c73e8bcf33258ba62cea92a27c1
Danny <74193127+llamaengineer@users.noreply.github.com>
2023-05-16 09:32:20 +0100

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,22 @@
+import { Spinner } from "@chakra-ui/react";
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+	gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+	const { data, error, isLoading } = useTrailers(gameId);
+
+	if (isLoading) <Spinner />;
+
+	if (error) throw error;
+
+	const first = data?.results[0];
+
+	return !first ? null : (
+		<video src={first.data[480]} poster={first.preview} controls />
+	);
+};
+
+export default GameTrailer;
